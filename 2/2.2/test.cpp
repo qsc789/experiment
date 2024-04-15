aee8a8cb37f023ee93326a7a050f2f0e5f96ce85
qsc789 <1279495882@qq.com>
2024-04-15 20:33:08 +0800

    2.2

---
+++ 2/2.2/test.cpp
@@ -0,0 +1,23 @@
+﻿#include"test.h"
+
+int main()
+{
+	int num = 0;
+	cout << "输入十进制数：";
+	cin >> num;
+	cout << "八进制数为：";
+	ST st;
+	StackInit(&st);
+	while (num != 0)
+	{
+		StackPush(&st, num % 8);
+		num /= 8;
+	}
+	while (!StackEmpty(&st))
+	{
+		cout << StackTop(&st);
+		StackPop(&st);
+	}
+	StackDestroy(&st);
+	return 0;
+}
