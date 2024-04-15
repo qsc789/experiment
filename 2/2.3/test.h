a391443d19145484cd4e39ef0a647adc7b4f09cb
qsc789 <1279495882@qq.com>
2024-04-15 20:57:18 +0800

    2.3

--- 2/2.3/test.h
+++ 2/2.3/test.h
@@ -23,24 +23,3 @@ int StackSize(ST* ps);
 bool StackEmpty(ST* ps);
 void StackPrint(ST ps);
 
-//—≠ª∑∂”¡–
-typedef int QDataType;
-typedef struct QueueNode
-{
-	struct QueueNode* next;
-	QDataType data;
-}QueueNode;
-typedef struct Queue
-{
-	QueueNode* head;
-	QueueNode* tail;
-
-}Queue;
-void QueueInit(Queue* pq);
-void QueueDestroy(Queue* pq);
-void QueuePush(Queue* pq, QDataType x);
-void QueuePop(Queue* pq);
-QDataType QueueFront(Queue* pq);
-QDataType QueueBack(Queue* pq);
-int QueueSize(Queue* pq);
-bool QueueEmpty(Queue* pq);
