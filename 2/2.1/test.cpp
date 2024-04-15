ebaed1c96303b1bd924e967114cde1a3e93f78f3
qsc789 <1279495882@qq.com>
2024-04-15 20:23:43 +0800

    2.1

---
+++ 2/2.1/test.cpp
@@ -0,0 +1,70 @@
+﻿#include"test.h"
+void TestStack1()
+{
+	ST st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+
+	//printf("%d", StackTop(&st));
+	StackDestroy(&st);
+}
+void TestStack2()
+{
+	ST st;
+	StackInit(&st);
+
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+	/*printf("%d ", StackTop(&st));
+	StackPop(&st);
+	printf("%d ", StackTop(&st));
+	StackPop(&st);*/
+	StackPush(&st, 5);
+	StackPush(&st, 6);
+
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);//��һ��
+	}
+
+	StackDestroy(&st);
+}
+
+void test3()
+{
+	Queue q;
+	QueueInit(&q);
+	/*QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+
+	QueuePop(&q);
+	QueuePop(&q);
+	QueuePop(&q);
+	QueuePop(&q);*/
+	//printf("%d\n", QueueBack(&q));
+	QueuePush(&q, 10);
+	QueuePush(&q, 20);
+
+	QueueDestroy(&q);
+}
+int main()
+{
+	//TestStack1();
+	//TestStack2();
+	test3();
+	return 0;
+}
