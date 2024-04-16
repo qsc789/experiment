bd4c51d9f49e795d72d622f9f11bee3a394e40b2
qsc789 <1279495882@qq.com>
2024-04-16 11:10:54 +0800

    2.5

---
+++ 2/2.5/test.cpp
@@ -0,0 +1,46 @@
+﻿#include"test.h"
+
+int main()
+{
+	ST st;
+	ST stemp;
+	StackInit(&st);
+	StackInit(&stemp);
+	char now;
+	int num, time;
+	while (1)
+	{
+		cin >> now >> num >> time;
+		if (now == 'E')
+		{
+			break;
+		}
+		point temp;
+		temp.now = now;
+		temp.num = num;
+		temp.time = time;
+		if (now == 'A')
+		{
+			StackPush(&st, temp);
+		}
+		else
+		{
+			while (num != StackTop(&st).num)
+			{
+				point t;
+				t = StackTop(&st);
+				StackPop(&st); 
+				StackPush(&stemp, t);
+			}
+			StackPop(&st);
+			while (!StackEmpty(&stemp))
+			{
+				StackPush(&st, StackTop(&stemp));
+				StackPop(&stemp);
+			}
+		}
+		StackPrint(st);
+	}
+	
+	return 0;
+}
---
+++ 2/2.5/test.h
@@ -0,0 +1,53 @@
+#pragma once
+#include<iostream>
+#include<map>
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<stdbool.h>
+using namespace std;
+//’ª
+typedef struct point
+{
+	char now;
+	int num;
+	int time;
+}point;
+typedef point STDataType;
+typedef struct Stack
+{
+	STDataType* a;
+	int top;
+	int capacity;
+}ST;
+
+void StackInit(ST* ps);
+void StackDestroy(ST* ps);
+void StackPush(ST* ps, STDataType x);
+void StackPop(ST* ps);
+STDataType StackTop(ST* ps);
+int StackSize(ST* ps);
+bool StackEmpty(ST* ps);
+void StackPrint(ST ps);
+
+//—≠ª∑∂”¡–
+typedef int QDataType;
+typedef struct QueueNode
+{
+	struct QueueNode* next;
+	QDataType data;
+}QueueNode;
+typedef struct Queue
+{
+	QueueNode* head;
+	QueueNode* tail;
+
+}Queue;
+void QueueInit(Queue* pq);
+void QueueDestroy(Queue* pq);
+void QueuePush(Queue* pq, QDataType x);
+void QueuePop(Queue* pq);
+QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
+int QueueSize(Queue* pq);
+bool QueueEmpty(Queue* pq);
