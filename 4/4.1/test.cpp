96bc835f9e688c477816fb69dacbdcf7d7b75311
qsc789 <1279495882@qq.com>
2024-04-23 08:22:37 +0800

    4.1

---
+++ 4/4.1/test.cpp
@@ -0,0 +1,27 @@
+﻿#include"test.h"
+//int index = 0;
+int main()
+{
+	
+	Queue q;
+	QueueInit(&q);
+	string tree;
+	cout << "前序输入树：";
+	getline(cin, tree);
+	
+	
+	BTNode* root=buildTree(tree);
+	cout<<"前序：";
+	PrevOrder(root);
+	cout << endl;
+	cout << "中序：";
+	InOrder(root);
+	cout << endl;
+	cout << "后序：";
+	BackOrder(root);
+	cout << endl;
+
+
+	
+	return 0;
+}
---
+++ 4/4.1/test.h
@@ -0,0 +1,40 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<stdbool.h>
+using namespace std;
+typedef char BTDataType;
+typedef struct BinaryTreeNode
+{
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+	BTDataType data;
+}BTNode;
+typedef char QDataType;
+typedef struct QueueNode
+{
+	struct QueueNode* next;
+	QDataType val;
+}QueueNode;
+typedef struct Queue
+{
+	QueueNode* head;
+	QueueNode* tail;
+
+}Queue;
+
+BTNode* buildTree(string preorder);
+void QueueInit(Queue* pq);
+void QueuePush(Queue* pq, QDataType temp);
+void QueuePop(Queue* pq);
+QDataType QueueFront(Queue* pq);
+bool QueueEmpty(Queue* pq);
+
+int maxDepth(BTNode* root);
+void PrevOrder(BTNode* root);
+void InOrder(BTNode* root);
+void BackOrder(BTNode* root);
